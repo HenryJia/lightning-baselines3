a2791400d60f88e5465739f6c560d7d04b87e77c
Hengjian Jia <henryjia18@gmail.com>
2021-01-12 06:02:35 +0000

    First draft of OffPolicyModel

--- lightning_baselines3/off_policy_models/off_policy_model.py
+++ lightning_baselines3/off_policy_models/off_policy_model.py
@@ -9,7 +9,7 @@ import torch
 import pytorch_lightning as pl
 
 from lightning_baselines3.common.base_model import BaseModel
-from lightning_baselines3.common.buffers import RolloutBuffer, RolloutBufferSamples
+from lightning_baselines3.common.buffers import ReplayBuffer, ReplayBufferSamples
 from lightning_baselines3.common.type_aliases import GymEnv
 from lightning_baselines3.common.utils import safe_mean
 from lightning_baselines3.common.vec_env import VecEnv
@@ -32,7 +32,7 @@ class OffPolicyModel(BaseModel):
         instead of action noise exploration (default: False)
     :param sde_sample_freq: (int) Sample a new noise matrix every n steps when using gSDE
         Default: -1 (only sample at the beginning of the rollout)
-    :param sde_sample_freq: (bool) Whether to store gradients in the RolloutBuffer
+    :param sde_sample_freq: (bool) Whether to store gradients in the ReplayBuffer
         Default: False
     :param create_eval_env: (bool) Whether to create a second environment that will be
         used for evaluating the agent periodically. (Only available when passing string for the environment)
@@ -48,9 +48,10 @@ class OffPolicyModel(BaseModel):
         eval_env: Union[GymEnv, str],
         batch_size: int = 256,
         buffer_length: int = int(1e6),
-        learning_starts: int = 100,
-        train_freq: Optional[int] = None,
-        episodes_per_rollout: Optional[int] = None,
+        warmup_length: int = 100,
+        train_freq: int = -1,
+        episodes_per_rollout: int = -1,
+        num_rollouts: int = 1,
         gradient_steps: int = 1,
         num_eval_episodes: int = 100,
         gamma: float = 0.99,
@@ -72,8 +73,9 @@ class OffPolicyModel(BaseModel):
             use_sde=use_sde,
         )
 
-        assert train_freq or episodes_per_rollout. "At least one of train_freq or episodes_per_rollout must be passed"
-        if train_freq and episodes_per_rollout:
+        assert self.env.num_envs == 1, "OffPolicyAlgorithm only support single environment at this stage"
+        assert train_freq > 0 or episodes_per_rollout > 0. "At least one of train_freq or episodes_per_rollout must be passed"
+        if train_freq > 0 and episodes_per_rollout > 0:
             warnings.warn(
                 "You passed a positive value for `train_freq` and `n_episodes_rollout`."
                 "Please make sure this is intended. "
@@ -85,15 +87,15 @@ class OffPolicyModel(BaseModel):
 
         self.batch_size = batch_size
         self.buffer_length = buffer_length
-        self.learning_starts = learning_starts
+        self.warmup_length = warmup_length
         self.train_freq = train_freq
         self.episodes_per_rollout = episodes_per_rollout
         self.gradient_steps = gradient_steps
-        self.epochs_per_rollout = epochs_per_rollout
+        self.num_rollouts = num_rollouts
         self.gamma = gamma
         self.gae_lambda = gae_lambda
 
-        self.rollout_buffer = RolloutBuffer(
+        self.replay_buffer = ReplayBuffer(
             buffer_length,
             self.observation_space,
             self.action_space,
@@ -103,29 +105,36 @@ class OffPolicyModel(BaseModel):
         )
 
 
+    def reset(self):
+        super(OffPolicyModel, self).reset()
+        self.num_timesteps = 0
+
+
     def train_dataloader(self):
         return OffPolicyDataloader(self)
 
 
-    def collect_rollouts(self) -> RolloutBufferSamples:
+    def collect_rollouts(self):
         assert self._last_obs is not None, "No previous observation was provided"
         # Sample new weights for the state dependent exploration
         if self.use_sde:
             self.reset_noise(self.env.num_envs)
 
-        for i in range(self.buffer_length):
+        i = 0
+        total_episodes = 0
+
+        while i < self.train_freq or total_episodes < self.episodes_per_rollout:
             if self.use_sde and self.sde_sample_freq > 0 and i % self.sde_sample_freq == 0:
                 # Sample a new noise matrix
                 self.reset_noise(self.env.num_envs)
 
-            with torch.no_grad():
-                # Convert to pytorch tensor, let Lightning take care of any GPU transfer
-                obs_tensor = torch.as_tensor(self._last_obs).to(device=self.device, dtype=torch.float32)
-                dist, values = self(obs_tensor)
-                actions = dist.sample()
-                log_probs = dist.log_prob(actions)
-
-            actions = actions.cpu().numpy()
+            if self.num_timesteps < self.warmup_length:
+                action = np.array([self.action_space.sample()])
+            else:
+                with torch.no_grad():
+                    # Convert to pytorch tensor, let Lightning take care of any GPU transfer
+                    obs_tensor = torch.as_tensor(self._last_obs).to(device=self.device, dtype=torch.float32)
+                    action = self.predict(obs_tensor, deterministic=False)
 
             # Rescale and perform action
             clipped_actions = actions
@@ -137,19 +146,17 @@ class OffPolicyModel(BaseModel):
 
             new_obs, rewards, dones, infos = self.env.step(clipped_actions)
 
-            # Give access to local variables
-
             if isinstance(self.action_space, gym.spaces.Discrete):
                 # Reshape in case of discrete action
                 actions = actions.reshape(-1, 1)
-            self.rollout_buffer.add(self._last_obs, actions, rewards, self._last_dones, values, log_probs)
+
+            self.replay_buffer.add(self._last_obs, new_obs, actions, rewards, dones)
+
             self._last_obs = new_obs
-            self._last_dones = dones
-
-        samples = self.rollout_buffer.finalize(values, dones)
-        self.rollout_buffer.reset()
-        return samples
-
+            i += 1
+            # Note: VecEnv might not return None, it might return [None] or something, remember to double check this!
+            if dones:
+                total_episodes += 1
 
 
 class OffPolicyDataloader:
@@ -159,18 +166,6 @@ class OffPolicyDataloader:
 
     def __iter__(self):
         for i in range(self.model.num_rollouts):
-            experiences = self.model.collect_rollouts()
-            observations, actions, old_values, old_log_probs, advantages, returns = experiences
-            for j in range(self.model.epochs_per_rollout):
-                k = 0
-                perm = torch.randperm(observations.shape[0], device=observations.device)
-                while k < observations.shape[0]:
-                    batch_size = min(observations.shape[0] - k, self.model.batch_size)
-                    yield RolloutBufferSamples(
-                        observations[perm[k:k+batch_size]],
-                        actions[perm[k:k+batch_size]],
-                        old_values[perm[k:k+batch_size]],
-                        old_log_probs[perm[k:k+batch_size]],
-                        advantages[perm[k:k+batch_size]],
-                        returns[perm[k:k+batch_size]])
-                    k += batch_size
+            self.model.collect_rollouts()
+            for j in range(self.model.gradient_steps):
+                yield self.replay_buffer.sample()
