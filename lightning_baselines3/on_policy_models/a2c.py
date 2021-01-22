Add minimal example for A2C and correct off by one error

lightning_baselines3/on_policy_models/a2c.py
@@ -65,13 +65,13 @@ class A2C(OnPolicyModel):
         self,
         env: Union[GymEnv, str],
         eval_env: Union[GymEnv, str],
-        buffer_length: int = 2048,
-        num_rollouts: int = 1,
-        batch_size: int = 64,
-        epochs_per_rollout: int = 10,
+        buffer_length: int = 5,
+        num_rollouts: int = 100,
+        batch_size: int = 128,
+        epochs_per_rollout: int = 1,
         num_eval_episodes: int = 100,
         gamma: float = 0.99,
-        gae_lambda: float = 0.95,
+        gae_lambda: float = 1.0,
         value_coef: float = 0.5,
         entropy_coef: float = 0.0,
         use_sde: bool = False,
@@ -107,11 +107,11 @@ class A2C(OnPolicyModel):
         log_probs = dist.log_prob(batch.actions)
         values = values.flatten()
 
-        advantages = batch.advantages
+        advantages = batch.advantages.detach()
         advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
 
         policy_loss = -(advantages * log_probs).mean()
-        value_loss = F.mse_loss(batch.returns, values)
+        value_loss = F.mse_loss(batch.returns.detach(), values)
         entropy_loss = -dist.entropy().mean()
 
         loss = policy_loss + self.value_coef * value_loss + self.entropy_coef * entropy_loss

lightning_baselines3/on_policy_models/on_policy_model.py
@@ -90,46 +90,49 @@ class OnPolicyModel(BaseModel):
 
     def collect_rollouts(self) -> RolloutBufferSamples:
         assert self._last_obs is not None, "No previous observation was provided"
-        # Sample new weights for the state dependent exploration
-        if self.use_sde:
-            self.reset_noise(self.env.num_envs)
-
-        self.eval()
-        for i in range(self.buffer_length):
-            if self.use_sde and self.sde_sample_freq > 0 and i % self.sde_sample_freq == 0:
-                # Sample a new noise matrix
+        with torch.no_grad():
+            # Sample new weights for the state dependent exploration
+            if self.use_sde:
                 self.reset_noise(self.env.num_envs)
 
-            with torch.no_grad():
+            self.eval()
+            for i in range(self.buffer_length):
+                if self.use_sde and self.sde_sample_freq > 0 and i % self.sde_sample_freq == 0:
+                    # Sample a new noise matrix
+                    self.reset_noise(self.env.num_envs)
+
                 # Convert to pytorch tensor, let Lightning take care of any GPU transfer
                 obs_tensor = torch.as_tensor(self._last_obs).to(device=self.device, dtype=torch.float32)
                 dist, values = self(obs_tensor)
                 actions = dist.sample()
                 log_probs = dist.log_prob(actions)
 
-            actions = actions.cpu().numpy()
+                actions = actions.cpu().numpy()
 
-            # Rescale and perform action
-            clipped_actions = actions
-            # Clip the actions to avoid out of bound error
-            if isinstance(self.action_space, gym.spaces.Box):
-                clipped_actions = np.clip(actions, self.action_space.low, self.action_space.high)
-            elif isinstance(self.action_space, gym.spaces.Discrete):
-                clipped_actions = actions.astype(np.int32)
+                # Rescale and perform action
+                clipped_actions = actions
+                # Clip the actions to avoid out of bound error
+                if isinstance(self.action_space, gym.spaces.Box):
+                    clipped_actions = np.clip(actions, self.action_space.low, self.action_space.high)
+                elif isinstance(self.action_space, gym.spaces.Discrete):
+                    clipped_actions = actions.astype(np.int32)
 
-            new_obs, rewards, dones, infos = self.env.step(clipped_actions)
+                new_obs, rewards, dones, infos = self.env.step(clipped_actions)
 
-            # Give access to local variables
+                # Give access to local variables
 
-            if isinstance(self.action_space, gym.spaces.Discrete):
-                # Reshape in case of discrete action
-                actions = actions.reshape(-1, 1)
-            self.rollout_buffer.add(self._last_obs, actions, rewards, self._last_dones, values, log_probs)
-            self._last_obs = new_obs
-            self._last_dones = dones
+                if isinstance(self.action_space, gym.spaces.Discrete):
+                    # Reshape in case of discrete action
+                    actions = actions.reshape(-1, 1)
+                self.rollout_buffer.add(self._last_obs, actions, rewards, self._last_dones, values, log_probs)
+                self._last_obs = new_obs
+                self._last_dones = dones
 
-        samples = self.rollout_buffer.finalize(values, dones)
-        self.rollout_buffer.reset()
+            final_obs = torch.as_tensor(new_obs).to(device=self.device, dtype=torch.float32)
+            dist, final_values = self(final_obs)
+            samples = self.rollout_buffer.finalize(final_values, dones)
+
+            self.rollout_buffer.reset()
         self.train()
         return samples
 

lightning_baselines3/on_policy_models/ppo.py
@@ -113,7 +113,7 @@ class PPO(OnPolicyModel):
         log_probs = dist.log_prob(batch.actions)
         values = values.flatten()
 
-        advantages = batch.advantages
+        advantages = batch.advantages.detach()
         advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
 
         ratio = torch.exp(log_probs - batch.old_log_probs)
@@ -122,9 +122,9 @@ class PPO(OnPolicyModel):
         policy_loss = -torch.min(policy_loss_1, policy_loss_2).mean()
 
         if self.clip_range_vf:
-            values = batch.old_values + torch.clamp(values - batch.old_values, -self.clip_range_vf, self.clip_range_vf)
+            values = batch.old_values.detach() + torch.clamp(values - batch.old_values.detach(), -self.clip_range_vf, self.clip_range_vf)
 
-        value_loss = F.mse_loss(batch.returns, values)
+        value_loss = F.mse_loss(batch.returns.detach(), values)
 
         entropy_loss = -dist.entropy().mean()
         loss = policy_loss + self.value_coef * value_loss + self.entropy_coef * entropy_loss